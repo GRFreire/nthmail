117766dd647e918e0b61cea0e5111438dbbf8e5e
Guilherme Rugai Freire <dev@grfreire.com>
2024-07-10 23:22:37 -0300

    update delete_old_mail.sh to use DB_PATH var

--- delete_old_mail.sh
+++ delete_old_mail.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+if [ -z "$DB_PATH" ]; then
+    DB_PATH="db.db"
+fi
+
 min_date="$(date -d '-1 day' +%s)"
 sql="DELETE FROM mails WHERE mails.arrived_at < $min_date;"
 
-echo "$sql" | sqlite3 db.db
+echo "$sql" | sqlite3 "$DB_PATH"
 
