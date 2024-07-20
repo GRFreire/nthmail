2ff581749003c52e168fe7c3435332fc14b26255
Guilherme Rugai Freire <dev@grfreire.com>
2024-07-20 00:26:12 -0300

    add an optinal rcpt_addr to not delete mails

--- delete_old_mail.sh
+++ delete_old_mail.sh
@@ -5,7 +5,12 @@ if [ -z "$DB_PATH" ]; then
 fi
 
 min_date="$(date -d '-1 day' +%s)"
-sql="DELETE FROM mails WHERE mails.arrived_at < $min_date;"
+sql="DELETE FROM mails WHERE mails.arrived_at < $min_date"
+
+if [ -n "$EXCLUDE_IGNORE_ADDR" ]; then
+    sql="$sql AND not (rcpt_addr = '$EXCLUDE_IGNORE_ADDR')"
+fi
+sql="$sql;"
 
 echo "$sql" | sqlite3 "$DB_PATH"
 
